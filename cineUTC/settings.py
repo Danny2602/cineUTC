0fa754c35624801f746290ebd3d37120ca14dddd
DESKTOP-51GC2CI\Danny <dannysimba2602@gmail.com>
2024-07-30 11:35:09 -0500

    cambios en yamp y settings host

--- cineUTC/settings.py
+++ cineUTC/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-pp5!=2ddigrx2ho8wexry%p*mhk^5&-yu5k7wm+hyzhvge19&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS= os.getenv('ALLOW_HOSTS','').split(',')    
+ALLOWED_HOSTS= os.getenv('ALLOW_HOSTS','cineutc-bx1z.onrender.com').split(',')    
 # ALLOWED_HOSTS = []
 
 
